2461d6aa4533c3d67ac9aaab7a5b5ac06fffd174
Patricia <patchley02@gmail.com>
2018-06-19 15:59:47 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,2 @@
-def over18 (age)
-  if age >= 18
-    puts "You're old enough!"
-  elsif age == 17
-    puts "You're SO close!"
-  else
-    puts "You're too young."
-  end
-end
+def fizzbuzz
 
-over18(17)
